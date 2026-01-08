faf9026d124b442dfa19f79748a55f7077b9ac1d
Mangar <Mangar@RYZEN9>
2026-01-08 10:02:53 +0100

    unit-test-helper includes adapted

--- src/test-system/unit/tournament-test-helpers.h
+++ src/test-system/unit/tournament-test-helpers.h
@@ -19,9 +19,9 @@
 
 #pragma once
 
-#include "../src\tournament.h"
-#include "../src\pair-tournament.h"
-#include "../chess-game\game-result.h"
+#include "../../tournament/tournament.h"
+#include "../../game-manager/pair-tournament.h"
+#include "../../chess-game/game-result.h"
 #include <vector>
 #include <optional>
 
