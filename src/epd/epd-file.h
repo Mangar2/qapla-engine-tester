f193ece237d4ca55f037b66856bcf8aa5c08ef6a
Mangar <Mangar@RYZEN9>
2026-02-09 08:38:30 +0100

    epd-file and spsa file added

---
+++ src/epd/epd-file.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace QaplaTester {
+
+/**
+ * @brief Definition of the EPD file format identifier.
+ */
+class EpdFile {
+public:
+    static constexpr const char* id = "epd";
+};
+
+}
---
+++ src/spsa/spsa-file.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace QaplaTester {
+
+/**
+ * @brief Definition of the SPSA file format identifier.
+ */
+class SpsaFile {
+public:
+    static constexpr const char* id = "spsa";
+};
+
+}
